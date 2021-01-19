6da0c44f2bdd54e5194e31f67c8adbdb92b3a92e
Gareth Pelly <garethpelly@currencyfair.com>
2021-01-19 15:44:28 +0000

    Updates Jenkinsfile to run terraform plan

--- .jenkins/release.groovy
+++ .jenkins/release.groovy
@@ -1,18 +1,62 @@
 node {
-   stage('Checkout') {
-     git branch: 'master',
-       credentialsId: 'fdb088af-f26a-47e8-b109-b49857442ef3',
-       url:'git@github.com:CurrencyFair/xsapi'
-   }
+    checkout()
+    build()
+    deploy()
+    cleanWorkspace()
+}
+
+def checkout() {
+    stage('Checkout') {
+        git branch: 'master',
+        credentialsId: 'fdb088af-f26a-47e8-b109-b49857442ef3',
+        url:'git@github.com:CurrencyFair/xsapi'
+    }
+}
+
+def build() {
    stage('Build') {
      sh 'npm i'
      sh 'mkdir -p ./dist'
      sh 'npm run build:docs'
    }
-   stage('Deploy to s3') {
-     sh 'aws s3 sync ./dist s3://cf-production-xsapi-reference-origin'
-   }
-   stage('Cleanup Workspace') {
-     cleanWs()
-   }
+}
+
+def deploy() {
+    stage('Deploy to s3') {
+        sh 'aws s3 sync ./dist s3://cf-production-xsapi-reference-origin'
+    }
+}
+
+def cleanWorkspace() {
+    stage('Cleanup Workspace') {
+        cleanWs()
+    }
+}
+
+def planInfrastructure() {
+    stage('Run Terraform Plan') {
+        def runid = ""
+        getWorkspaceId()
+//         buildPayload()
+//
+//         runid = startPlan()
+// 
+//         waitForPlan(runid)
+    }
+}
+
+def getWorkspaceId() {
+    withCredentials([string(credentialsId: 'jenkins-terraform-cloud', variable: 'jenkins-terraform-cloud']) {
+        def response = httpRequest(
+            customHeaders: [
+                    [ name: "Authorization", value: "Bearer " + env.BEARER_TOKEN ],
+                    [ name: "Content-Type", value: "application/vnd.api+json" ]
+                ],
+            url: "https://app.terraform.io/api/v2/organizations/" + env.TF_ORGNAME + "/workspaces/" + env.TF_WORKSPACE
+        )
+    }
+
+    def data = new JsonSlurper().parseText(response.content)
+    println ("Workspace Id: " + data.data.id)
+    return data.data.id
 }
